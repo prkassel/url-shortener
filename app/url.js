e6648f1b457e666e119c9bbf078dd093ef2c31ea
prkassel <prkassel@gmail.com>
2016-05-07 06:58:53 +0000

    heroku deployment

--- app/url.js
+++ app/url.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 var urlModel = require('../models/url');
 
-router.get('/', function(req, res) {
+router.route('/').get(function(req, res) {
   res.render('index');
 });
 
-router.get('/:id', function(req, res) {
-
+router.route('/:id').get(function(req, res) {
     urlModel.findById(req.params.id, function(err, url) {
         if (err) {
             res.send(err);
         }
+        else {
             res.redirect(url.original_url);
+        }
     });
 });
 module.exports = router;
